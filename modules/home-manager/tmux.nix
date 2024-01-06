03f89027ea97ab5e1f097d6dc6ad0106b5151dea
Malte kleine Piening <mkl.piening@gmail.com>
2024-01-06 16:09:09 +0100

    added tmux config

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,14 +1,14 @@
 { pkgs, config, lib, ... }: 
 let
-  # declare plugins
+  # my slightly customized version of catppuccin
   catppuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
     pluginName = "catppuccin";
-    version = "unstable-2022-12-14";
+    version = "2023-11-01";
     src = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "tmux";
-      rev = "e2561decc2a4e77a0f8b7c05caf8d4f2af9714b3";
-      sha256 = "sha256-6UmFGkUDoIe8k+FrzdzsKrDHHMNfkjAk0yyc+HV199M=";
+      owner = "mklpiening";
+      repo = "catppuccin_tmux";
+      rev = "4bbfe07f974f85b2844742eb55bc9abf8099a1bd";
+      sha256 = "sha256-ZdE3dHUTWQYczg0kROJRk4Y9+sOzBTPqVm9k2fz3WNs=";
     };
     postInstall = ''
       sed -i -e 's|''${PLUGIN_DIR}/catppuccin-selected-theme.tmuxtheme|''${TMUX_TMPDIR}/catppuccin-selected-theme.tmuxtheme|g' $target/catppuccin.tmux
@@ -18,23 +18,53 @@ in {
   # tmux config
   programs.tmux = {
     enable = true;
+    baseIndex = 1;
     clock24 = true;
     terminal = "screen-256color";
+    historyLimit = 100000;
     plugins = [
-      catppuccin
+      {                             # pastel colore theme
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
+
+          set -g @catppuccin_status_modules_right "directory date_time"
+          set -g @catppuccin_status_modules_left "session"
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator " "
+          set -g @catppuccin_status_right_separator_inverse "no"
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+
+          set -g @catppuccin_directory_text "#{b:pane_current_path}"
+          set -g @catppuccin_date_time_text "%H:%M"
+        '';
+      }
+      pkgs.tmuxPlugins.sensible     # some common settings
+      pkgs.tmuxPlugins.yank         # copy to system clipboard
+      {                             # save and restore tmux
+        plugin = pkgs.tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      # pkgs.tmuxPlugins.continuum    # automatic tmux save and restore
+      pkgs.tmuxPlugins.tmux-thumbs  # search current tmux view and use the selection for autofill
+      pkgs.tmuxPlugins.tmux-fzf     # fzf to manage tmux sessions, windows and panes
     ];
     extraConfig = ''
       set-option -g terminal-overrides ',xterm-256color:RGB'
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'tmux-plugins/tmux-sensible'
-      set -g @plugin 'tmux-plugins/tmux-yank'
-      set -g @plugin 'tmux-plugins/tmux-resurrect'
-      set -g @plugin 'tmux-plugins/tmux-continuum'
-      set -g @plugin 'fcsonline/tmux-thumbs'
-      set -g @plugin 'sainnhe/tmux-fzf'
-      set -g @plugin 'wfxr/tmux-fzf-url'
-      set -g @plugin 'omerxx/catppuccin-tmux' # My fork that holds the meetings script bc I'm lazy af
-      set -g @plugin 'omerxx/tmux-sessionx'
+
+      set -g renumber-windows on       # renumber all windows when any window is closed
+      set -g set-clipboard on          # use system clipboard
+      set -g status-position top       # macOS / darwin style
     '';
   };
 }
