efc468040efd8a66910cfe5355872c50eeabdf1c
Malte kleine Piening <mkl.piening@gmail.com>
2024-01-06 11:22:53 +0100

    added exemplary plugin

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,11 +1,30 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, lib, ... }: 
+let
+  # declare plugins
+  catppuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "unstable-2022-12-14";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "tmux";
+      rev = "e2561decc2a4e77a0f8b7c05caf8d4f2af9714b3";
+      sha256 = "sha256-6UmFGkUDoIe8k+FrzdzsKrDHHMNfkjAk0yyc+HV199M=";
+    };
+    postInstall = ''
+      sed -i -e 's|''${PLUGIN_DIR}/catppuccin-selected-theme.tmuxtheme|''${TMUX_TMPDIR}/catppuccin-selected-theme.tmuxtheme|g' $target/catppuccin.tmux
+    '';
+  };
+in {
+  # tmux config
   programs.tmux = {
     enable = true;
     clock24 = true;
     terminal = "screen-256color";
+    plugins = [
+      catppuccin
+    ];
     extraConfig = ''
       set-option -g terminal-overrides ',xterm-256color:RGB'
-      
       set -g @plugin 'tmux-plugins/tpm'
       set -g @plugin 'tmux-plugins/tmux-sensible'
       set -g @plugin 'tmux-plugins/tmux-yank'
