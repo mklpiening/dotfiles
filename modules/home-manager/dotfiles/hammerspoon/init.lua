10c51745d79b954cee97ebbbdd7244dae17bd541
Malte kleine Piening <mkl.piening@gmail.com>
2024-02-12 23:20:26 +0100

    added chris-style hammerspoon shortcuts

--- modules/home-manager/dotfiles/hammerspoon/init.lua
+++ modules/home-manager/dotfiles/hammerspoon/init.lua
@@ -1,4 +1,4 @@
-local hyper = { "cmd", "alt", "ctrl", "shift" }
+local hyper = { "cmd", "shift" }
 
 -- We use 0 to reload the configuration
 hs.hotkey.bind(hyper, "0", function()
@@ -7,3 +7,26 @@ end)
 
 -- Notify about the config reload
 hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
+
+local applicationHotkeys = {
+	--------------------
+	-- LEFT HAND
+	w = "iTerm",
+	e = "Visual Studio Code",
+	a = "Arc",
+	r = "Foxglove Studio",
+	f = "Finder",
+	c = "FreeCAD",
+	g = "Gather",
+
+	--------------------
+	-- RIGHT HAND
+	n = "Obsidian",
+	m = "Spotify",
+}
+
+for key, app in pairs(applicationHotkeys) do
+	hs.hotkey.bind(hyper, key, function()
+		hs.application.launchOrFocus(app)
+	end)
+end
