8628f73c3ec43a9c0c0e82f1a701959c9d91ed60
Malte kleine Piening <mkl.piening@gmail.com>
2024-07-31 13:25:20 +0200

    added git squash command

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -84,8 +84,9 @@
 
     aliases = {
       l1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
-	    l2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
-	    l = "!git l1";
+      l2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all";
+      l = "!git l1";
+      squash = "!f(){ git reset --soft HEAD~\${1} && git commit --edit -m\"\$(git log --format=%B --reverse HEAD..HEAD@{1})\"; };f";
     };
 
     extraConfig = {
